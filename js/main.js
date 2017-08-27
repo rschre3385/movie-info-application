9436d7288c8edab500df68a30fe7253f15207272
Ryan Schreder <schredermail@yahoo.com>
2017-08-27 12:46:02 -0500

    second commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-  axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=thewdb')
+  axios.get('https://www.omdbapi.com?s='+searchText+'&apikey=thewdb')
     .then((response) => {
       console.log(response);
       let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id){
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
 
-  axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
+  axios.get('https://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
     .then((response) => {
       console.log(response);
       let movie = response.data;
